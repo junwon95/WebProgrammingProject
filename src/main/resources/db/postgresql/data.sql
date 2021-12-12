7c816874b7abd6ab788c43b08ca680d0e818ef30
서준원 <junwon@seojun-won-ui-MacBookPro.local>
2021-12-12 20:25:09 +0900

    update

--- src/main/resources/db/postgresql/data.sql
+++ src/main/resources/db/postgresql/data.sql
@@ -1,15 +1,23 @@
-INSERT INTO specialties VALUES (1, 'radiology') ON CONFLICT (id) DO NOTHING;
-INSERT INTO specialties VALUES (2, 'surgery') ON CONFLICT (id) DO NOTHING;
-INSERT INTO specialties VALUES (3, 'dentistry') ON CONFLICT (id) DO NOTHING;
+INSERT INTO specialties VALUES (1, '영상의학') ON CONFLICT (id) DO NOTHING;
+INSERT INTO specialties VALUES (2, '수술전문') ON CONFLICT (id) DO NOTHING;
+INSERT INTO specialties VALUES (3, '치과의사') ON CONFLICT (id) DO NOTHING;
 
-
-INSERT INTO vets VALUES (1, 'James','',1, 1) ON CONFLICT (id) DO NOTHING;
+INSERT INTO vets VALUES (1, '동국이','동국대 출신 의사',1, 1) ON CONFLICT (id) DO NOTHING;
+INSERT INTO vets VALUES (2, '홍길동','길동역에 살고있어요',3, 2) ON CONFLICT (id) DO NOTHING;
+INSERT INTO vets VALUES (3, '제임스','한국말 잘 모릅니다.',4, 3) ON CONFLICT (id) DO NOTHING;
 
 INSERT INTO owners VALUES (1, 'George' , 2) ON CONFLICT (id) DO NOTHING;
 
 INSERT INTO members VALUES (1, 'test', '123', 'junwonseo95@gmail.com', 'VET') ON CONFLICT (id) DO NOTHING;
 INSERT INTO members VALUES (2, 'test2', '123', 'ssmmttpp123@gmail.com', 'USER') ON CONFLICT (id) DO NOTHING;
+INSERT INTO members VALUES (3, 'test3', '123', 'ssmmttpp12@gmail.com', 'VET') ON CONFLICT (id) DO NOTHING;
+INSERT INTO members VALUES (4, 'test4', '123', 'ssmmttpp1@gmail.com', 'VET') ON CONFLICT (id) DO NOTHING;
 
-INSERT INTO pets VALUES (1, 'Leo', '2000-09-07', 'cat', 1) ON CONFLICT (id) DO NOTHING;
+INSERT INTO pets VALUES (1, '멍멍이', '2000-09-07', '개', 1) ON CONFLICT (id) DO NOTHING;
+INSERT INTO pets VALUES (2, '초이', '2000-09-07', '고양이', 1) ON CONFLICT (id) DO NOTHING;
 
-INSERT INTO visits VALUES (1, 1, 1, '2010-03-04', 'rabies shot') ON CONFLICT (id) DO NOTHING;
+INSERT INTO visits VALUES (1, 1, 1, '2010-03-04', '배가 아파요') ON CONFLICT (id) DO NOTHING;
+INSERT INTO visits VALUES (2, 2, 1, '2010-01-04', '머리가 아파요') ON CONFLICT (id) DO NOTHING;
+INSERT INTO visits VALUES (3, 1, 1, '2010-01-04', '뚱뚱해요') ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO treatments VALUES (1, 1, 1, '할미손이 약손', '복통약 처방') ON CONFLICT (id) DO NOTHING;
